6af86173ef79039ae8368f4358de5bbf77652b33
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-11-20 16:28:14 +0200

    v. 0.3.2

--- routes/middleware/validationBody.js
+++ routes/middleware/validationBody.js
@@ -1,9 +1,5 @@
 const validationBody = (schema) => {
     return function (req, res, next) {
-        // const emptyReqBody = Object.keys(req.body).length === 0;
-        // if (emptyReqBody) {
-        //     throw new Error("Missing fields");
-        // };
         const validateBody = schema.validate(req.body);
         if (validateBody.error) {
             next(validateBody.error);
